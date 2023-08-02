b7edf162f067be34f113d3b8017cdcadd9f0710b
Sergey Kachaev <metallian@yandex.ru>
2023-08-02 14:19:51 +0300

    we made it!

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,7 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
 import web.service.CarService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/cars")
@@ -18,8 +22,15 @@ public class CarController {
 
 
     @GetMapping
-    public String showCars (Model model){
-        model.addAttribute("cars", cs.getAllCars());
+    public String showCars (@RequestParam(name="count", required = false) Integer count, Model model) {
+        List<Car> allCars = cs.getAllCars();
+
+        if (count != null && count < allCars.size()) {
+            List<Car> displayedCars = allCars.subList(0, count);
+            model.addAttribute("cars", displayedCars);
+        } else {
+            model.addAttribute("cars", allCars);
+        }
         return "cars";
     }
 
