9a700802af92d29486aba071050e7fa843949825
Sergey Kachaev <metallian@yandex.ru>
2023-08-02 13:09:58 +0300

    made a controller and car model

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,4 +1,17 @@
 package web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cars")
 public class CarController {
+
+    @GetMapping
+    public String showCars (Model model){
+        return "cars";
+    }
+
 }
